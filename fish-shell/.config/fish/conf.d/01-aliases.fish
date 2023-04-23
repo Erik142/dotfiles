1843acc79951cdab9d5e22885a350decd1d19a10
Erik Wahlberger <ewahlberger@gmail.com>
2023-04-23 16:23:49 +0200

    add Renode alias

--- fish-shell/.config/fish/conf.d/01-aliases.fish
+++ fish-shell/.config/fish/conf.d/01-aliases.fish
@@ -3,5 +3,8 @@ alias p=project
 alias ls=exa
 alias ll='ls -l'
 alias tree='ls -T'
+if test (uname) = Darwin
+  alias renode='/Applications/Renode.app/Contents/MacOS/macos_run.command --console --disable-xwt'
+end
 alias config="vim $HOME/.config/fish/conf.d"
 alias reload="source $HOME/.config/fish/conf.d/*.fish"
