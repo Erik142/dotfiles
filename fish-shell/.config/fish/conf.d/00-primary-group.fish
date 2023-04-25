f398a01c340a18a349ce3debfa71606956a9570f
Erik Wahlberger <ewahlberger@gmail.com>
2023-04-25 17:26:21 +0200

    fish: fix primary-group not working

--- fish-shell/.config/fish/conf.d/00-primary-group.fish
+++ fish-shell/.config/fish/conf.d/00-primary-group.fish
@@ -1,7 +1,7 @@
 # set -g primary_group "admin"
 
 if set -q primary_group
-  if test "(id -g -nr)" != "$primary_group"
+  if test (id -g -nr) != "$primary_group"
     if test (uname) = "Linux"
       exec sg "$primary_group" "$SHELL $argv"
     end
