892f10c1684249babd3f962b86275fdecc2769c0
Erik Wahlberger <ewahlberger@gmail.com>
2024-09-27 17:05:29 +0200

    fix(fish): enable graphics in renode alias

--- fish-shell/.config/fish/conf.d/01-aliases.fish
+++ fish-shell/.config/fish/conf.d/01-aliases.fish
@@ -4,7 +4,7 @@ alias ls=eza
 alias ll='ls -l'
 alias tree='ls -T'
 if test (uname) = Darwin
-  alias renode='/Applications/Renode.app/Contents/MacOS/macos_run.command --console --disable-xwt'
+  alias renode='/Applications/Renode.app/Contents/MacOS/macos_run.command --console'
 end
 alias config="vim $HOME/.config/fish/conf.d"
 alias reload="source $HOME/.config/fish/conf.d/*.fish"
