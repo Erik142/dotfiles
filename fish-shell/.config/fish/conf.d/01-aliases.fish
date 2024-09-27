e3f0d4b9f86671c48c2d9b02e4436a7e4a77425f
Erik Wahlberger <ewahlberger@gmail.com>
2024-09-27 17:05:53 +0200

    fix(fish): add cleardns alias on Mac OS X

--- fish-shell/.config/fish/conf.d/01-aliases.fish
+++ fish-shell/.config/fish/conf.d/01-aliases.fish
@@ -5,6 +5,7 @@ alias ll='ls -l'
 alias tree='ls -T'
 if test (uname) = Darwin
   alias renode='/Applications/Renode.app/Contents/MacOS/macos_run.command --console'
+  alias cleardns='sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder'
 end
 alias config="vim $HOME/.config/fish/conf.d"
 alias reload="source $HOME/.config/fish/conf.d/*.fish"
