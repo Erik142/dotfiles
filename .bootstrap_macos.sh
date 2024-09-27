07353920aef214a66c87db1493bb0fda17918c9a
Erik Wahlberger <ewahlberger@gmail.com>
2024-09-27 17:14:51 +0200

    fix(bootstrap): symlink realpath to gnu realpath for Mac OS X

--- .bootstrap_macos.sh
+++ .bootstrap_macos.sh
@@ -10,10 +10,19 @@ function install_homebrew() {
 
 }
 
+# Needed for the tmux_autoreload plugin to work properly
+function create_realpath_symlink() {
+  if [ ! -e "/opt/homebrew/bin/realpath" ]; then
+    echo "Adding symlink to realpath..."
+    ln -s /opt/homebrew/bin/grealpath /opt/homebrew/bin/realpath
+  fi
+}
+
 function main() {
   install_homebrew
   export PATH="$PATH:/opt/homebrew/bin"
   brew bundle
+  create_realpath_symlink
 }
 
 main
