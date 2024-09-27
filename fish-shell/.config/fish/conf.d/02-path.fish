6c2a4813ad9900f8eda03a435fe316c333acdd6a
Erik Wahlberger <ewahlberger@gmail.com>
2024-09-27 17:09:16 +0200

    fix(fish): change order of path initialization

--- fish-shell/.config/fish/conf.d/02-path.fish
+++
@@ -1,5 +0,0 @@
-if status is-interactive
-  fish_add_path -g $HOME/.cargo/bin
-  fish_add_path $HOME/.local/bin /opt/local/bin /opt/local/sbin /opt/homebrew/opt/python/libexec/bin $HOME/bin $HOME/.luarocks/bin
-  fish_add_path /opt/homebrew/opt/llvm/bin
-end
---
+++ fish-shell/.config/fish/conf.d/03-path.fish
@@ -0,0 +1,10 @@
+fish_add_path -g $HOME/Library/Python/3.11/bin
+if status is-interactive
+  set -gx PYENV_ROOT $HOME/.pyenv
+  fish_add_path /opt/homebrew/lib/ruby/gems/3.1.0/bin/ /opt/homebrew/opt/ruby@3.1/bin $HOME/.local/bin /opt/local/bin /opt/local/sbin /opt/homebrew/opt/python/libexec/bin $HOME/bin $HOME/.luarocks/bin /Users/erikwahlberger/.gem/ruby/3.1.0/bin /opt/homebrew/opt/gnu-sed/libexec/gnubin
+  fish_add_path /opt/homebrew/opt/llvm/bin
+  fish_add_path /opt/homebrew/opt/gnu-getopt/bin
+  fish_add_path -m $PYENV_ROOT/shims
+  fish_add_path -g $HOME/.cargo/bin
+  fish_add_path -g $HOME/go/bin
+end
