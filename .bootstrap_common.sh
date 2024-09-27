3d654ffd95a2f5e9f5a718bea021dab29c3d9739
Erik Wahlberger <ewahlberger@gmail.com>
2024-09-27 17:22:11 +0200

    fix(bootstrap): download catppuccin bat themes

--- .bootstrap_common.sh
+++ .bootstrap_common.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+
+function download_bat_themes() {
+  mkdir -p "$(bat --config-dir)/themes"
+
+  wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Latte.tmTheme
+  wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Frappe.tmTheme
+  wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Macchiato.tmTheme
+  wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
+
+  bat cache --build
+}
+
+function main() {
+  download_bat_themes
+}
+
+main
